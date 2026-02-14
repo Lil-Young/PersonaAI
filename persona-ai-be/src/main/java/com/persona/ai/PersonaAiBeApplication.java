8dcadf701f438888f0e3f5833bf3fc00226bb234
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2026-02-15 02:11:22 +0900

    feat: Initial commit
    프론트엔드/백엔드 프로젝트 초기 세팅

---
+++ persona-ai-be/src/main/java/com/persona/ai/PersonaAiBeApplication.java
@@ -0,0 +1,13 @@
+package com.persona.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaAiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonaAiBeApplication.class, args);
+    }
+
+}
---
+++ persona-ai-be/src/test/java/com/persona/ai/PersonaAiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persona.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaAiBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
